d00273326c05b41370c6273c873930918907c53c
Adi Prastiyo <adiprass28@gmail.com>
2022-02-28 03:24:26 +0800

    Add Navbar folder

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Navbar } from './components'
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
@@ -12,7 +14,7 @@ const App = () => {
       <Testimonial />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
