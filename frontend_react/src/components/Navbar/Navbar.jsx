0d76b3952b559ee32542e66c119ebed55f4eb579
Adi Prastiyo <adiprass28@gmail.com>
2022-02-28 04:18:31 +0800

    Added file in assets folder

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
