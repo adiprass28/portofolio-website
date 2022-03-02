1b2652674c2968d177d151d669f7da3e6103c5bc
Adi Prastiyo <adiprass28@gmail.com>
2022-03-03 06:18:49 +0800

    Add framer-motion on Navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import { images } from '../../constants'
 import './Navbar.scss'
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -17,6 +21,25 @@ function Navbar() {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85 }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              <li key={item}>
+                <div />
+                <a href={`#${item}`}>{item}</a>
+              </li>
+            ))}
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }
